0655112f5bf992b2d78ad0e7cc2ec17eabdabd99
Glêsio Santos <glesioss@gmail.com>
2024-12-11 08:37:31 -0300

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup></script>
 
 <template>
-  <div class="flex">
+  <div class="container">
     <img src="./assets/logo.png"/>
+    <hr>
+    <h1>Microphones can b used not only to pick up sound, but also to project to a speaker.</h1>
+
+    <label><input type="radio" name="option" value="true"/>True</label>
+    <label> <input type="radio" name="option" value="false"/>False</label>
+    <button>Enviar</button>
   </div>
+
 </template>
 
 <style scoped></style>
